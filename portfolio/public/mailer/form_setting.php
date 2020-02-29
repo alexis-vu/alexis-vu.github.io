e9a78be7d545099c13accfad84f04af9263f3447
Alexis Vu <alexis.lauren.vu@gmail.com>
2020-02-29 01:54:23 -0800

    Implemented new theme.

---
+++ portfolio/public/mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Perssonal Website'"; //letter subject
+	$to = 'alexis.lauren.vu@gmail.com'; // Enter Your E-mail
+	$from = 'admin@alexis-vu.github.io'; // Admin e-mail
+	$fromName = 'Alexis Vu'; // Your company name
+	$charset = 'UTF-8';
+?>
--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 
 // import BrowserRouter, Route, and Link components
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 // import components
-import Header from './Header.js';
-import Home from './Home.js';
-import About from './About.js';
-import Projects from './Projects.js';
-import Contact from './Contact.js';
-import Footer from './Footer.js';
-import PhotoswipeBG from './PhotoswipeBG.js';
 
 // import css files
-import './base.css';
-import './fonts.css';
-import './main.css';
-import './vendor.css';
+import './style.css';
+import './table.css';
+import './glitche-basic.css';
+import './glitche-layout.css';
+import './ionicons.css';
+import './magnific-popup.css';
+import './animate.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header></Header>
-        <Home></Home>
-        <About></About>
-        {/* <Projects></Projects>*/ }
-        <Contact></Contact>
-        <Footer></Footer>
-        <PhotoswipeBG></PhotoswipeBG>
       </div>
     </BrowserRouter>
   );
